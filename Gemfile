f0c93c270074df044849ea84e29dee8638242555
Graham <graham@dentedtriangle.com>
2012-03-25 18:31:39 +0800

    initial setup

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+# base
+gem 'rails', '3.2.2'
+
+# ui
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'uglifier'
+  gem 'html5-rails'
+  gem 'jquery-rails'
+  gem 'bootstrap-sass'
+end
+
+# database / models
+gem 'decent_exposure'
+gem 'mongoid'
+gem 'bson_ext', :require => 'mongo'
+
+gem 'devise'
+
+# deployment
+gem 'heroku'
+gem 'thin'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playlist::Application.config.secret_token = 'be82042d812e3fe5a3aa428a90197135a47f1d6d699a27e16b18c075c0846891f8f7ac024a8a497b369e090827c86c8d9f6ad27340cd580adb513405918192c8'
